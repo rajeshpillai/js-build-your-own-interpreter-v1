09a25e6da36bf9ebcc9c05749d35adf353ec42f5
rajeshpillai <pillai.rajesh@gmail.com>
2022-10-04 20:19:20 +0530

    basic code structure for the interpreter

---
+++ eva.js
@@ -0,0 +1,29 @@
+const assert = require('assert');
+
+class Eva {
+  eval(exp) {
+    if (isNumber(exp)) {
+      return exp;
+    }
+    if (isString(exp)) {
+      return exp.slice(1,-1);
+    }
+    throw 'Unimplemented';
+  }
+
+}
+
+function isNumber(exp) {
+  return typeof exp === 'number';
+}
+
+function isString(exp) {
+  return typeof exp === 'string' && exp[0] === '"' && exp.slice(-1) === '"';
+}
+
+const eva = new Eva();
+assert.strictEqual(eva.eval(1), 1);
+assert.strictEqual(eva.eval('"Hello"'), "Hello");
+assert.strictEqual(eva.eval(['+',1,5]), 6);
+
+console.log('All assertions passed.');
