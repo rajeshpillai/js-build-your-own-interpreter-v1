d1997ee60cbd9b32bf6bf9ae78a8398e27c326e2
rajeshpillai <pillai.rajesh@gmail.com>
2022-10-04 21:03:08 +0530

    more test case for var evaluation

--- eva.js
+++ eva.js
@@ -102,6 +102,10 @@ assert.strictEqual(eva.eval('VERSION'), '0.1');
 // var isUser = true;
 assert.strictEqual(eva.eval(['var', 'isUser', 'true']), true);
 
+
+// More test for variable evaluation
+assert.strictEqual(eva.eval(['var', 'z', ['*', 2,2]]), 4);
+
 console.log('All assertions passed.');
 
 
